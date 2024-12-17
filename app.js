9fde5faa2b26c2ac3e3644c8f70d792bd4fd731f
Waseem Khalil <waseemkhalilworld@gmail.com>
2024-12-17 10:36:18 -0800

    uderstanding Q/A

---
+++ app.js
@@ -0,0 +1,62 @@
+//First Question 1
+
+// function abc() {
+//     console.log(abc.xyz);
+// }
+// abc()
+// abc.xyz=300
+// abc.xyz=400
+// abc()
+
+//Second Question 2
+
+// const Numbers=[1,2,3,4]
+// Numbers[100]=500
+// console.log(Numbers)
+
+//Third Question 3
+
+// console.log(typeof typeof 100)
+
+//Fourth Question 4
+
+// const arr=[...`Waseem`]
+// console.log(arr);
+
+//Fifth Question 5
+
+// console.log(parseInt("1-2"));
+// console.log(parseInt('7FM'));
+// console.log(parseInt('M7F'));
+
+//Six Question 6
+
+// console.log(isNaN('Waseem'))
+
+//Sevean Question 7
+
+// console.log(
+//   [1, 2].map((num) => {
+//     if (num > 0) return;
+//     console.log("hi running");
+//   })
+// );
+
+//Eight Question 8
+
+// function abc(){
+//     return;
+// }
+// console.log(abc());
+
+//Nine Qustion 9
+
+// "use strict";
+// {
+//   function abc() {
+//     console.log("Waseem");
+//   }
+// }
+// abc();
+
+// Ten Question 10
